07112621e7b67158a3220c614b5bc61cce0960ec
ssigridss-dot <ssigridss@gmail.com>
2026-05-30 15:56:44 +0300

    added models

---
+++ RecipeCollectionAPI/Models/Recipe.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeCollectionAPI.Models;
+
+public class Recipe
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; } = "";
+
+    [Required]
+    [MaxLength(100)]
+    public string Category { get; set; } = "";
+
+    [Required]
+    public string Ingredients { get; set; } = "";
+
+    [Required]
+    public string Instructions { get; set; } = "";
+
+    [Range(1, 1000)]
+    public int CookingTime { get; set; }
+}
